4ec9c4b8da47d42db14269a15ca6db2d529ed6f4
pratyush kumar <pratyushbndm@gmail.com>
2023-08-28 10:54:25 +0530

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+ import React from 'react'
+import Navbar from './Navbar'
+import Textedit from './Textedit'
+ 
+ export default function App() {
+   return (
+     <div>
+      <Navbar/>
+      <Textedit/> 
+     </div>
+   )
+ }
+ 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import img from '../img/img.png'
+
+export default function Navbar() {
+  return (
+    <div className='navbar'>
+        <div className="nav">
+            <img src={img} alt="" />
+        </div>
+      
+    </div>
+  )
+}
